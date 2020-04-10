af54779a5af6fdd494e508dcb3306a886253f547
yeonnseok <ljinsk3@gmail.com>
2020-04-10 14:26:46 +0900

    feat: 최소 수량 최대 수량 범위 벗어날 경우 예외 발생

---
+++ src/main/java/domain/Quantity.java
@@ -0,0 +1,38 @@
+package domain;
+
+import java.util.Objects;
+
+public class Quantity {
+    public static final int MIN_QUANTITY = 1;
+    public static final int MAX_QUANTITY = 99;
+
+    private final int quantity;
+
+    public Quantity(final int quantity) {
+        if (quantity < MIN_QUANTITY | quantity > MAX_QUANTITY) {
+            throw new IllegalArgumentException("메뉴당 최소 1개, 최대 99개까지 주문 가능 합니다.");
+        }
+        this.quantity = quantity;
+    }
+
+    public Quantity addQuantity(final int value) {
+        return new Quantity(quantity + value);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Quantity quantity1 = (Quantity) o;
+        return quantity == quantity1.quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(quantity);
+    }
+}
---
+++ src/test/java/domain/QuantityTest.java
@@ -0,0 +1,42 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class QuantityTest {
+
+    @DisplayName("최소 수량(=1), 최대 수량(=99) 범위를 벗어날 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1, 0, 100, 1000})
+    void constructorMin(int value) {
+        assertThatThrownBy(() -> {
+            Quantity quantity = new Quantity(value);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("메뉴당 최소 1개, 최대 99개까지 주문 가능 합니다.");
+    }
+
+    @DisplayName("같은 메뉴 주문 추가 시 수량 증가")
+    @ParameterizedTest
+    @CsvSource({"1,51", "2,52", "3,53", "4,54", "49,99"})
+    void addQuantity(int value, int expectedQuantity) {
+        Quantity quantity = new Quantity(50);
+        Quantity nextQuantity = quantity.addQuantity(value);
+        assertThat(nextQuantity).isEqualTo(new Quantity(expectedQuantity));
+    }
+
+    @DisplayName("같은 메뉴 주문 추가 시 수량 99 초과할 경우 예외 발생")
+    @Test
+    void addQuantityFail() {
+        Quantity quantity = new Quantity(50);
+        assertThatThrownBy(() -> {
+            Quantity newQuantity = quantity.addQuantity(50);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("메뉴당 최소 1개, 최대 99개까지 주문 가능 합니다.");
+    }
+}
